ebd48ead556c956cf876316a7f037290a059ea0b
HendrikCromboom <hcromboom@gmail.com>
2020-08-13 23:56:47 +0200

    added elders

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function update(){
     document.title = soulcount + "Souls";
     document.getElementById("amountAcolytes").innerHTML = "There are" + acolytes + "Acolytes in your covent";
     document.getElementById("costRecruitAcolyte").innerHTML = ((acolytes + 1) *12) + "Souls";
+    document.getElementById("amountElders").innerHTML = "There are" + elders + "Elders in your covent";
+    document.getElementById("costRecruitElders").innerHTML = ((elders + 1) *120) + "Souls";
 }
 var soulcount = 0;
 var acolytes = 0;
+var elders= 0;
 function timer(){
     soulcount = soulcount + acolytes;
+    soulcount = soulcount + elders *2;
     update()
 }
 setInterval(timer, 1000)
@@ -23,14 +27,25 @@ function recruitAcolyte(){
        update();
    }
 }
+function recruitElder(){
+    if (soulcount >= ((elders + 1) *120)){
+        soulcount = soulcount - ((elders + 1) *120);
+        elders = elders + 1;
+        update();
+    }
+}
 function save(){
+    localStorage.setItem("Sicko", "Cheater")
     localStorage.setItem("soulcount", soulcount);
     localStorage.setItem("acolytes", acolytes);
+    localStorage.setItem("elders", elders);
 }
 function load(){
     soulcount = localStorage.getItem("soulcount");
     soulcount = parseInt(soulcount);
     acolytes = localStorage.getItem("acolytes");
     acolytes = parseInt(acolytes);
+    elders = localStorage.getItem("elders");
+    elders = parseInt(elders);
     update();
 }
